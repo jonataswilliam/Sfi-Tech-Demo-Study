ec147b9270deb0e85af14d887edbeea8d842337f
Jonatas William <jonatas_2602@hotmail.com>
2018-04-29 15:43:43 -0300

    Add Mouse Look com Inversao de Controles e Sensibilidade

---
+++ Assets/Game/_Scripts/Look.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour {
+
+	[SerializeField]
+	private float sensitivity = 1f;
+	[SerializeField]
+	private bool invertYRotation = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float _mouseX = Input.GetAxis("Mouse X");
+		float _mouseY = Input.GetAxis("Mouse Y");
+
+		// Para pegar o valor de rotacao de um eixo individual utilizamos Euler e não Quartenium.
+		// Armazenando valores do tranform atual em uma variável temporária
+		Vector3 newRotation = transform.localEulerAngles;
+
+		// Aplicando valor apenas no eixo desejado;
+		newRotation.y += _mouseX * sensitivity;
+		// Verifica se a rotacao para Y esta setada para invertida
+		if(invertYRotation) {			
+			newRotation.x += _mouseY * sensitivity;
+		} else {
+			newRotation.x -= _mouseY * sensitivity;
+		}	
+
+		// Atualizando valores do transform inteiro.
+		transform.localEulerAngles = newRotation;
+	}
+}
---
+++ Assets/Game/_Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private CharacterController _controller;
+
+	[SerializeField]
+	private float _speed = 3.5f;
+	private float _gravity = 9.81f; //Gravidade da Terra
+
+	// Use this for initialization
+	void Start () {	
+		_controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CalculateMove();
+	}
+
+	void CalculateMove() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		
+		// Guarda direcao de acordo com a entrada no controle do jogador
+		Vector3 direction = new Vector3( horizontal, 0, vertical);
+		Vector3 velocity = direction * _speed;		
+
+		//Aplica gravidade no personagem
+		velocity.y -= _gravity;
+
+		// Transformando valores que alteram o Local Space para alteração do World Space.
+		velocity = transform.transform.TransformDirection(velocity);	
+		
+ 
+		// Aplica Movimentação
+		_controller.Move(velocity * Time.deltaTime);
+	}
+
+}
