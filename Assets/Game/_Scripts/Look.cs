0479f36be05784365193a08ee1e6c4a11a586388
Jonatas William <jonatas_2602@hotmail.com>
2018-04-30 14:07:04 -0300

    Correcao de movimentacao da camera

--- Assets/Game/_Scripts/Look.cs
+++ Assets/Game/_Scripts/Look.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Look : MonoBehaviour {
 
 	[SerializeField]
-	private float sensitivity = 1f;
+	private float _sensitivity = 1f;
 	[SerializeField]
 	private bool invertYRotation = false;
 	// Use this for initialization
@@ -20,18 +20,20 @@ public class Look : MonoBehaviour {
 
 		// Para pegar o valor de rotacao de um eixo individual utilizamos Euler e não Quartenium.
 		// Armazenando valores do tranform atual em uma variável temporária
-		Vector3 newRotation = transform.localEulerAngles;
+		Vector3 newRotationX = transform.eulerAngles;
+		Vector3 newRotationY = transform.localEulerAngles;
 
 		// Aplicando valor apenas no eixo desejado;
-		newRotation.y += _mouseX * sensitivity;
+		newRotationX.y += _mouseX * _sensitivity;
 		// Verifica se a rotacao para Y esta setada para invertida
 		if(invertYRotation) {			
-			newRotation.x += _mouseY * sensitivity;
+			newRotationY.x += _mouseY * _sensitivity;
 		} else {
-			newRotation.x -= _mouseY * sensitivity;
+			newRotationY.x -= _mouseY * _sensitivity;
 		}	
 
 		// Atualizando valores do transform inteiro.
-		transform.localEulerAngles = newRotation;
+		transform.eulerAngles = newRotationX;
+		transform.localEulerAngles = newRotationY;
 	}
 }
